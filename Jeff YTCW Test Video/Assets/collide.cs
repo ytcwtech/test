975f662c0211986c3d5ae1e146a6da4c11be7cd4
ytcwtech <59745455+ytcwtech@users.noreply.github.com>
2020-01-29 15:20:10 -0500

    testing
    testing

---
+++ Jeff YTCW Test Video/Assets/collide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+}
